9bab6d76677c32656e6137dc34c2c14415847557
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2020-11-25 15:47:42 +0800

    setup ref

--- vue3-1/src/App.vue
+++ vue3-1/src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to JSPang.com" />
+    <div>
+      <h2>欢迎光临红浪漫洗浴中心</h2>
+      <div>请选择一位美女为你服务</div>
+    </div>
+    <div>
+      <button
+        v-for="(item, index) in girls"
+        v-bind:key="index"
+        @click="selectGirlFun(index)"
+      >
+        {{ index }} : {{ item }}
+      </button>
+    </div>
+    <div>你选择了【{{ selectGirl }}】为你服务</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
+  setup() {
+    const girls = ref(["大脚", "刘英", "晓红"]);
+    const selectGirl = ref("");
+    const selectGirlFun = (index: number) => {
+      selectGirl.value = girls.value[index];
+    };
+    return {
+      girls,
+      selectGirl,
+      selectGirlFun,
+    };
   },
 });
 </script>
