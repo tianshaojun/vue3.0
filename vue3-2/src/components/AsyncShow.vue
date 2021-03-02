7f5c1fdd706286c8a96d1c1187f2d5601315cdf8
田少军 <tianshaojun@tianshaojundeMacBook-Pro.local>
2021-03-02 20:58:05 +0800

    20210302commit

---
+++ vue3-2/src/components/AsyncShow.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ result }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    setup() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                return resolve({result:"JSPang"});
+            },2000);
+        });
+    },
+});
+</script>
